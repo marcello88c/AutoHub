bc6672a6190b882f01d69a979891bef2acdeade2
marcello88c <102691404+marcello88c@users.noreply.github.com>
2024-04-26 22:21:25 +0100

    Add files via upload

--- templates/ModelPage/Main/index.tsx
+++ templates/ModelPage/Main/index.tsx
@@ -7,13 +7,12 @@ const Main = ({}: MainProps) => (
     <div className="pt-20 md:pt-15">
         <div className="container">
             <div className="max-w-[50rem] mb-30 text-display xl:mb-25">
-                The amazing experience of the newest model of carsova electric
-                cars.
+                The exceptional sensation provided by the latest iteration of Carsova electric vehicles is truly remarkable.
             </div>
-            <div className="flex justify-between mb-6 text-h5 md:mb-4 md:text-body">
+            {/* <div className="flex justify-between mb-6 text-h5 md:mb-4 md:text-body">
                 <div className="">The Electrical Carsova - DMG A 80x</div>
                 <div className="shrink-0 ml-6">208 - 188 g/km</div>
-            </div>
+            </div> */}
             <div className="relative rounded-[1.25rem] overflow-hidden before:absolute before:inset-0 before:z-1 before:bg-radial-gradient before:opacity-40">
                 <Image
                     className="w-full md:min-h-[33.125rem] md:object-cover"
